33c09bcf6a9d63352ba63010dac4224ad8b0579a
Adrian Badiu <adrian.badiu@mail.com>
2018-10-15 07:29:27 +0100

    🎉 Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Midnight Coder — a <a href='https://github.com/kyleamathews/typography.js'>Typography.js</a> theme
+
+A theme for Typography.js inspired by the typography of the Midnight Coder default theme
+
+## Install
+```bash
+npm install --save typography typography-theme-midnight-coder
+```
+## Usage
+```javascript
+import Typography from 'typography'
+import midnightCoderTheme from 'typography-theme-midnight-coder'
+
+const typography = new Typography(midnightCoderTheme)
+```
+## More on theme
+
+Midnight Coder uses "<a href='https://fonts.google.com/specimen/Merriweather'>Merriweather</a>" for headers and "<a href='https://fonts.google.com/specimen/Merriweather'>Merriweather</a>" for body text
---
+++ src/index.js
@@ -0,0 +1,93 @@
+// @flow
+import gray from 'gray-percentage'
+import type { OptionsType } from 'Types'
+import { MOBILE_MEDIA_QUERY } from 'typography-breakpoint-constants'
+
+const theme: OptionsType = {
+  title: 'Midnight Coder',
+  baseFontSize: '16px',
+  baseLineHeight: 1.75,
+  scaleRatio: 5 / 2,
+  googleFonts: [
+    {
+      name: 'Source Code Pro',
+      styles: ['700'],
+    },
+    {
+      name: 'Open Sans',
+      styles: ['400', '400i', '700', '700i', '900', '900i'],
+    },
+  ],
+  headerFontFamily: ['Source Code Pro', 'serif'],
+  bodyFontFamily: ['Open Sans', 'sans-serif'],
+  bodyColor: 'hsla(0, 0%, 87%, 1)',
+  headerWeight: 900,
+  bodyWeight: 400,
+  boldWeight: 700,
+  overrideStyles: ({ adjustFontSizeTo, scale, rhythm }, options) => ({
+    h1: {
+      fontFamily: ['Source Code Pro', 'sans-serif'].join(','),
+    },
+    blockquote: {
+      ...scale(1 / 5),
+      color: gray(41),
+      fontStyle: 'italic',
+      paddingLeft: rhythm(13 / 16),
+      marginLeft: rhythm(-1),
+      borderLeft: `${rhythm(3 / 16)} solid ${gray(10)}`,
+    },
+    'blockquote > :last-child': {
+      marginBottom: 0,
+    },
+    'blockquote cite': {
+      ...adjustFontSizeTo(options.baseFontSize),
+      color: options.bodyColor,
+      fontWeight: options.bodyWeight,
+    },
+    'blockquote cite:before': {
+      content: '"— "',
+    },
+    ul: {
+      listStyle: 'disc',
+    },
+    'ul,ol': {
+      marginLeft: 0,
+    },
+    [MOBILE_MEDIA_QUERY]: {
+      'ul,ol': {
+        marginLeft: rhythm(1),
+      },
+      blockquote: {
+        marginLeft: rhythm(-3 / 4),
+        marginRight: 0,
+        paddingLeft: rhythm(9 / 16),
+      },
+    },
+    'h1,h2,h3,h4,h5,h6': {
+      marginTop: rhythm(2),
+    },
+    h4: {
+      letterSpacing: '0.140625em',
+      textTransform: 'uppercase',
+    },
+    h6: {
+      fontStyle: 'italic',
+    },
+    a: {
+      boxShadow: '0 1px 0 0 currentColor',
+      color: '#66D9EF',
+      textDecoration: 'none',
+    },
+    'a:hover,a:active': {
+      boxShadow: 'none',
+    },
+    'mark,ins': {
+      background: '#66D9EF',
+      color: 'white',
+      padding: `${rhythm(1 / 16)} ${rhythm(1 / 8)}`,
+      textDecoration: 'none',
+    },
+  }),
+}
+
+export default theme
